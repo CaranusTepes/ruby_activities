57d9181f492a3a5fa71f79fd1353e8578f721621
Janssen Radh Yumang <janssenreal23@gmail.com>
2022-05-21 10:09:07 +0800

    edited solution

--- activity_6.rb
+++ activity_6.rb
@@ -1,14 +1,4 @@
 def is_isogram(string)
-hash = {}
-
-string.each_char do |char |
-    char = char.downcase
-
-  if hash[char]
-return false
-end
-hash[char] = true
-
-end
-return true
+  string = string.downcase
+    ('a'..'z').none?{|letter| string.count(letter) > 1}
 end
