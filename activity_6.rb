09bfe13c137f469ef2d854cf6da45f0fb36e4817
Janssen Radh Yumang <janssenreal23@gmail.com>
2022-05-21 10:14:19 +0800

    done isogram

--- activity_6.rb
+++ activity_6.rb
@@ -2,3 +2,9 @@ def is_isogram(string)
   string = string.downcase
     ('a'..'z').none?{|letter| string.count(letter) > 1}
 end
+
+# string.downcase!
+# ('a'..'z').each do |letter|
+#  return false if string.count(letter) > 1
+# end
+# return true
